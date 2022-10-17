f2b7d6be4c8ea1e5dced1052a02cba568aa3bd9e
Yusuf Erdem <yusuf@thundra.io>
2022-10-17 20:13:24 +0300

    refactor for review comments

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-}
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
